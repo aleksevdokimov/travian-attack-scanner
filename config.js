a3108fe42b247699fb91c3388484fe8aaf2c59d2
aleksevdokimov <alex.rassom@google.com>
2026-04-09 16:50:35 +0300

    фикс локаный адрес

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-  API_URL: 'http://94.131.105.41',
+  //API_URL: 'http://94.131.105.41',
+  API_URL: 'http://127.0.0.1:8000',
   DEBUG: true, // Включаем отладочное логирование
   MIN_AUTO_SCAN_INTERVAL: 2000, // 2 секунды между автосканированиями
   
